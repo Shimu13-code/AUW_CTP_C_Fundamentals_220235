a6250459c2127e32e14bec47b876a3cb6152d1ad
Shimu13-code <jarin.shimu@auw.edu.bd>
2026-02-22 20:33:21 +0600

    Add files via upload

---
+++ q26_220235.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("Enter a number: ");
+
+    if (n > 0)
+    {
+        printf("Positive\n");
+    }
+    else if (n < 0)
+    {
+        printf("Negative\n");
+    }
+    else
+    {
+        printf("Zero\n");
+    }
+}
